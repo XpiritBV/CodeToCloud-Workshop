d9c704402f136c0cdf1aa3143411dfec7f49509a
Rene van Osnabrugge <rvanosnabrugge@live.com>
2020-09-29 11:34:04 +0200

    changed in text and settings

--- Containers/ghws-fix/Debug.ps1
+++ Containers/ghws-fix/Debug.ps1
@@ -1,9 +1,9 @@
 Set-Location $PSScriptRoot
 
-$env:settingsLocation = ".\settings.json"
-$env:ACTION = "Solution"
-$env:EXERCISE = "DEVWF-T004"
-$env:SOURCEREPO = "https://github.com/XpiritBV/CodeToCloud-Workshop"
+$env:settingsLocation = ".\settings.json" 
+$env:ACTION = "Solution" #matches with the folder 
+$env:EXERCISE = "DEVWF-T004" 
+$env:SOURCEREPO = "https://github.com/XpiritBV/CodeToCloud-Workshop" #This is the repo where all the solution files can be found.. Hardcoded in Docker file
 
 #Load Settings
 . ..\base-ghws-pwsh\init-helpers.ps1
