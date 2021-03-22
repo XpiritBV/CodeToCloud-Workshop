1514bcb6ddeda7313eb4e1941a52ffbb56c0c2d7
Jesse Houwing <jesse.houwing@gmail.com>
2021-03-22 15:42:59 +0100

    Workspaces root folder differs between local and remote.

--- Sources/.devcontainer/Microsoft.PowerShell_profile.ps1
+++ Sources/.devcontainer/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,17 @@
-$workshopPath = Get-ChildItem -Path "/home/codespace/workspace/" -Include ".workshop" -Recurse -Directory -Hidden | select-object -ExpandProperty "FullName" { $_.FullName }
+$workspacePaths = @("/home/codespace/workspace/", "/workspaces/")
+$workshopPath = ""
+
+foreach ($path in $workspacePaths){
+    if (Test-Path $path){
+        $workshopPath = Get-ChildItem -Path $path -Include ".workshop" -Recurse -Directory -Hidden | select-object -ExpandProperty "FullName" { $_.FullName }
+        break
+    }
+}
+
+if (-not $workshopPath){
+    Write-Error "Workspace folder not in the expected location."
+}
+
 $setupScript = join-path $workshopPath "setup.ps1"
 $workshopStepScript = join-path $workshopPath "workshop-step.ps1"
 
